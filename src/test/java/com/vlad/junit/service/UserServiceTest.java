0bb8c136b68f48a82eedbbfecbd8a86e8104ecb2
Vlad Krivonos <vlad58r@gmail.com>
2026-02-12 21:48:09 +0300

    Created first test and setting maven

---
+++ src/test/java/com/vlad/junit/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.vlad.junit.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
