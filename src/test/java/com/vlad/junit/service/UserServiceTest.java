eea9eaf06774b168664cf58d9898f87ae74b5058
Vlad Krivonos <vlad58r@gmail.com>
2026-02-12 23:09:45 +0300

    Add test lifecycle

--- src/test/java/com/vlad/junit/service/UserServiceTest.java
+++ src/test/java/com/vlad/junit/service/UserServiceTest.java
@@ -1,15 +1,51 @@
 package com.vlad.junit.service;
 
-import org.junit.jupiter.api.Test;
+import com.vlad.junit.dto.User;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    void initAll() {
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNotUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
         assertTrue(users.isEmpty(), () -> "User list should be empty");//assertFalse
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDataBase() {
+        System.out.println("After each: " + this);
+        System.out.println();
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
     }
 }
